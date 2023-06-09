4e81388629c102e7bab8704c50d610d5659bdd8e
Himanshu vyas <43252344+RoadRomeo1@users.noreply.github.com>
2023-06-09 11:52:38 +0530

    Update CheckPrimeNumber.java

--- project_alpha/src/com/project_alpha/utilities/CheckPrimeNumber.java
+++ project_alpha/src/com/project_alpha/utilities/CheckPrimeNumber.java
@@ -2,30 +2,23 @@ package com.project_alpha.utilities;
 
 public class CheckPrimeNumber {
 
-	public static boolean isPrimeNumber(long number) {
-		boolean status=false;
+	    public static boolean isPrimeNumber(long number) {
+		boolean status=true;
        
-		if(number==0||number==1) {
+		if(number<=0||number==1) {
 			status=false;
 		}
 		else {
-			long temp=number/2;
-			int flag=0;
-			
-			for(int i=2;i<=temp;i++) {
-				if(number%i==0) {
-					status=false;
-					flag++;
-					break;
-				}
-			}
-			if(flag==0) {
-				status=true;
-			}
-		}
-		
-		return status;
+          		  for(int i = 2; i < number; i++){
+               		   if(number%i == 0 && number != i){
+                    	   status = false;
+                    	   break;
+                	}	
+            	     }
 	}
+    
+		return status;
+}
 	
 	public static void main(String[] args) {
 		/*Main method implementation starts....
