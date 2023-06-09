b82a22ead8d5c8fdcf4822364f61b5d3894e9a15
Himanshu vyas <43252344+RoadRomeo1@users.noreply.github.com>
2023-06-09 12:04:41 +0530

    Update CheckPrimeNumber.java

--- project_alpha/src/com/project_alpha/utilities/CheckPrimeNumber.java
+++ project_alpha/src/com/project_alpha/utilities/CheckPrimeNumber.java
@@ -1,24 +1,20 @@
 package com.project_alpha.utilities;
 
+import java.util.stream.LongStream;
+
 public class CheckPrimeNumber {
 
-	    public static boolean isPrimeNumber(long number) {
-		boolean status=true;
-       
-		if(number<=0||number==1) {
-			status=false;
-		}
-		else {
-          		  for(int i = 2; i < number; i++){
-               		   if(number%i == 0 && number != i){
-                    	   status = false;
-                    	   break;
-                	}	
-            	     }
-	}
-    
-		return status;
-}
+public static boolean isPrimeNumber(long number){
+        boolean status = true;
+
+        if(number>=1)
+            status = false;
+
+        status = !LongStream.range(2, number).filter(n -> (number%n == 0) && (number != n)).findAny().isPresent();
+        
+        return status;
+    }
+
 	
 	public static void main(String[] args) {
 		/*Main method implementation starts....
